e20a3d9d1b62d613e52ffb9a806c86fce52d6b5f
Selwyn Preston <selwyn.preston@hackney.gov.uk>
2019-06-11 14:31:28 +0100

    Started work on implementing customer information gateway

--- customer-information-api.Tests/V1/Gateways/CustomerInformationGatewayTests.cs
+++ customer-information-api.Tests/V1/Gateways/CustomerInformationGatewayTests.cs
@@ -2,26 +2,50 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bogus;
+using customer_information_api.Tests.V1.Helper;
+using customer_information_api.V1.Domain;
 using customer_information_api.V1.Gateways;
+using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
+using UnitTests;
 
 namespace customer_information_api.Tests.V1.Gateways
 {
     [TestFixture()]
-    public class CustomerInformationGatewayTests
+    public class CustomerInformationGatewayTests : DbTest
     {
+        private readonly Faker _faker = new Faker();
         private CustomerInformationGateway _classUnderTest;
         [SetUp]
         public void SetUp()
         {
-            _classUnderTest = new CustomerInformationGateway();
+            _classUnderTest = new CustomerInformationGateway(_uhContext);
+        }
+
+        [Test]
+        public void ListOfCustomersImplementBoundaryInterface()
+        {
+            Assert.NotNull(_classUnderTest is ICustomerInformationGateway);
         }
 
         [Test]
         public void CallingTheGatewayShouldReturnNullIfNoMatches()
         {
-
+            var  response = _classUnderTest.GetCustomerInformationByTagReference("Test");
+            Assert.AreEqual(0, response.Count);
+            Assert.AreEqual(null, response.FirstOrDefault());
         }
+
+        [Test]
+        public void CallingTheGatewayShouldReturnACollectionOfCustomers()
+        {
+            CustomerInformation customerInformation = CustomerInformationHelper.CreateCustomerInformation();
+            DbSet<CustomerInformation>
+            _uhContext.Database.
+        }
+
+
     }
 }
---
+++ customer-information-api/V1/Infrastructure/UhCustomerInformation.cs
@@ -0,0 +1,49 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace customer_information_api.V1.Infrastructure
+{
+    [Table("member")]
+    public class UhCustomerInformation
+    {
+        [Column("title")] public string Title { get; set; }
+        [Column("forename")] public string Forename { get; set; }
+        [Column("surname")] public string Surname { get; set; }
+        [Column("ni_no")] public string Ni_No { get; set; }
+        [Column("dob")] public string DOB { get; set; }
+        [Column("gender")] public string Gender { get; set; }
+        [Column("house_ref")] public string HouseRef { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((UhCustomerInformation) obj);
+        }
+
+        protected bool Equals(UhCustomerInformation other)
+        {
+            return string.Equals(Title, other.Title) &&
+                   string.Equals(Forename, other.Forename) &&
+                   string.Equals(Surname, other.Surname) &&
+                   string.Equals(Ni_No, other.Ni_No) &&
+                   string.Equals(DOB, other.DOB) &&
+                   string.Equals(Gender, other.Gender) &&
+                   string.Equals(HouseRef, other.HouseRef)
+        }
+
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (HouseRef != null ? HouseRef.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Title != null ? Title.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ Comments.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
+}
