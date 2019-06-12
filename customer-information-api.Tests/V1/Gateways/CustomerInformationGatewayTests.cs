0c26b931ff0140f77162092efe2b0ef4e35e4e2e
Selwyn Preston <selwyn.preston@hackney.gov.uk>
2019-06-12 13:50:15 +0100

    APIF-165 #comment Added text fixture for calling the Cust Info Gateway

--- customer-information-api.Tests/V1/Gateways/CustomerInformationGatewayTests.cs
+++ customer-information-api.Tests/V1/Gateways/CustomerInformationGatewayTests.cs
@@ -6,6 +6,7 @@ using Bogus;
 using customer_information_api.Tests.V1.Helper;
 using customer_information_api.V1.Domain;
 using customer_information_api.V1.Gateways;
+using customer_information_api.V1.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
@@ -42,6 +43,37 @@ namespace customer_information_api.Tests.V1.Gateways
         public void CallingTheGatewayShouldReturnACollectionOfCustomers()
         {
             CustomerInformation customerInformation = CustomerInformationHelper.CreateCustomerInformation();
+
+            UhCustomerInformation dbCustomerInformation =
+                UhCustomerInformationHelper.CreateUhCustomerInformationFrom(customerInformation);
+
+            UhAgreement dbAgreement = new UhAgreement()
+            {
+                HouseRef = dbCustomerInformation.HouseRef,
+                TagRef = "000035/01",
+                Active = _faker.Random.Bool(),
+                AdditionalDebit = _faker.Random.Bool(),
+                Committee =  _faker.Random.Bool(),
+                CourtApp =  _faker.Random.Bool(),
+                DtStamp = _faker.Date.Past(),
+                Eviction =  _faker.Random.Bool(),
+                FdCharge =  _faker.Random.Bool(),
+                FreeActive =  _faker.Random.Bool(),
+                OtherAccounts =  _faker.Random.Bool(),
+                Terminated =  _faker.Random.Bool(),
+                IntroDate = _faker.Date.Past(),
+                ReceiptCard =  _faker.Random.Bool(),
+                IntroExtDate = _faker.Date.Past(),
+                PotentialEndDate = _faker.Date.Soon(),
+                UPaymentExpected = _faker.Random.AlphaNumeric(3)
+            };
+            _uhContext.uHCustomerInformations.Add(dbCustomerInformation);
+            _uhContext.uHAgreements.Add(dbAgreement);
+            _uhContext.SaveChanges();
+
+            var response = _classUnderTest.GetCustomerInformationByTagReference(dbCustomerInformation.HouseRef);
+            Assert.NotNull(response.FirstOrDefault());
+            Assert.AreEqual(dbAgreement.TagRef, response.FirstOrDefault().tenancyRef);
         }
 
 
