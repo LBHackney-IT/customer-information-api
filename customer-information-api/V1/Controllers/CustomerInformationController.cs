f89a9d557bf187910e2dd59686c77361a619635e
Liudvikas Taluntis <Liudvikas.Taluntis@hackney.gov.uk>
2019-06-13 14:47:02 +0100

    APIF-167 #comment Fixed the issue, where the wrong data from an endpoint call would be returned. Issue was that there were two endpoints with exact same path that differed only by the variable at the end.

--- customer-information-api/V1/Controllers/CustomerInformationController.cs
+++ customer-information-api/V1/Controllers/CustomerInformationController.cs
@@ -49,21 +49,21 @@ namespace customer_information_api.V1.Controllers
         }
 
 
-        /// <summary>
-        /// Returns a single customer information record for a customer id.
-        /// </summary>
-        /// <param name="customer_id"></param>
-        /// <returns></returns>
-        [HttpGet]
-        [Route("{customer_id}")]
-        [Produces("application/json")]
-        [ProducesResponseType(typeof(GetSingleCustomerInformationResponse), 200)]
-        [ProducesResponseType(typeof(CustomerInformationBadRequest), 400)]
-        [ProducesResponseType(typeof(NotFoundResult), 404)]
-        public IActionResult GetCustomerInformationByCustomerId([FromRoute] string customer_id)
-        {
-            return new JsonResult(new CustomerInformation()) { StatusCode = 501 };
-        }
+        ///// <summary>
+        ///// Returns a single customer information record for a customer id.
+        ///// </summary>
+        ///// <param name="customer_id"></param>
+        ///// <returns></returns>
+        //[HttpGet]
+        //[Route("{customer_id}")]
+        //[Produces("application/json")]
+        //[ProducesResponseType(typeof(GetSingleCustomerInformationResponse), 200)]
+        //[ProducesResponseType(typeof(CustomerInformationBadRequest), 400)]
+        //[ProducesResponseType(typeof(NotFoundResult), 404)]
+        //public IActionResult GetCustomerInformationByCustomerId([FromRoute] string customer_id)
+        //{
+        //    return new JsonResult(new CustomerInformation()) { StatusCode = 501 };
+        //}
     }
 
     internal class CustomerInformationBadRequest
